d643d7d1e14cb41cad9783c54982322b2c03d318
Phil Wareham <phil@designhive.com>
2013-11-16 14:55:51 +0000

    Might as well jshint the Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,6 +4,7 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-compass');
     grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
 
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
@@ -31,12 +32,42 @@ module.exports = function (grunt) {
                     {expand: true, cwd: 'src/img/', src: ['**'], dest: 'dist/textpattern/images/'}
                 ]
             }
+        },
+
+        jshint: {
+            files: ['Gruntfile.js'],
+            options: {
+                bitwise: true,
+                camelcase: true,
+                curly: true,
+                eqeqeq: true,
+                es3: true,
+                forin: true,
+                immed: true,
+                indent: 4,
+                latedef: true,
+                noarg: true,
+                noempty: true,
+                nonew: true,
+                quotmark: 'single',
+                undef: true,
+                unused: true,
+                strict: true,
+                trailing: true,
+                browser: true,
+                globals: {
+                    $: false,
+                    jQuery: false,
+                    module: true
+                }
+            }
         }
 
     });
 
+    grunt.registerTask('test', ['jshint']);
     grunt.registerTask('sass', ['compass']);
     grunt.registerTask('default', ['watch']);
-    grunt.registerTask('build', ['sass', 'copy']);
-    grunt.registerTask('travis', ['compass']);
+    grunt.registerTask('build', ['jshint', 'sass', 'copy']);
+    grunt.registerTask('travis', ['jshint', 'compass']);
 };
