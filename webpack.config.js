e4f4873e3aa7445887aa66c4f517857b2f00d697
Phil Wareham <phil@designhive.com>
2017-12-12 10:06:47 +0000

    Build minifed and unminified CSS files

--- webpack.config.js
+++ webpack.config.js
@@ -6,52 +6,82 @@ var CleanWebpackPlugin = require('clean-webpack-plugin');
 var StyleLintPlugin = require('stylelint-webpack-plugin');
 var OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
-module.exports = {
-    entry: {
-        'jquery-ui.css': './scss/jquery-ui.scss'
-    },
-    output: {
-        path: distDir,
-        filename: '.Trashes'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(sass|scss)$/,
-                use: ExtractTextPlugin.extract({
-                    fallback: 'style-loader',
-                    use: [
-                        { loader: 'css-loader', options: { minimize: false, importLoaders: 2 } },
-                        { loader: 'postcss-loader', options: { plugins: [require('autoprefixer')] } },
-                        { loader: 'sass-loader', options: { outputStyle: 'expanded', precision: 7 } }
-                    ]
-                })
-            },
-            {
-                test: /\.css$/,
-                loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
-            },
-        ]
-    },
-    plugins: [
-        new CleanWebpackPlugin(distDir),
-        new StyleLintPlugin({
-            configFile: '.stylelintrc.yml',
-            syntax: 'scss',
-            context: 'scss',
-            files: '**/*.scss',
-            failOnError: false,
-            quiet: false
-        }),
-        new ExtractTextPlugin('jquery-ui.css'),
-        new OptimizeCssAssetsPlugin({
-            assetNameRegExp: /\.css$/g,
-            cssProcessorOptions: {
-                safe: true,
-                discardComments: {
-                    removeAll: true
+module.exports = [
+    {
+        entry: {
+            'jquery-ui.min.css': './scss/jquery-ui.scss'
+        },
+        output: {
+            path: distDir,
+            filename: '.Trashes'
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.(sass|scss)$/,
+                    use: ExtractTextPlugin.extract({
+                        fallback: 'style-loader',
+                        use: [
+                            { loader: 'css-loader', options: { minimize: false, importLoaders: 2 } },
+                            { loader: 'postcss-loader', options: { plugins: [require('autoprefixer')] } },
+                            { loader: 'sass-loader', options: { outputStyle: 'expanded', precision: 7 } }
+                        ]
+                    })
+                }, {
+                    test: /\.css$/,
+                    loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
+                },
+            ]
+        },
+        plugins: [
+            new CleanWebpackPlugin(distDir),
+            new StyleLintPlugin({
+                configFile: '.stylelintrc.yml',
+                syntax: 'scss',
+                context: 'scss',
+                files: '**/*.scss',
+                failOnError: false,
+                quiet: false
+            }),
+            new ExtractTextPlugin('jquery-ui.min.css'),
+            new OptimizeCssAssetsPlugin({
+                assetNameRegExp: /\.css$/g,
+                cssProcessorOptions: {
+                    safe: true,
+                    discardComments: {
+                        removeAll: true
+                    }
                 }
-            }
-        }),
-    ]
-};
+            }),
+        ]
+    }, {
+        entry: {
+            'jquery-ui.css': './scss/jquery-ui.scss'
+        },
+        output: {
+            path: distDir,
+            filename: '.Trashes'
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.(sass|scss)$/,
+                    use: ExtractTextPlugin.extract({
+                        fallback: 'style-loader',
+                        use: [
+                            { loader: 'css-loader', options: { minimize: false, importLoaders: 2 } },
+                            { loader: 'postcss-loader', options: { plugins: [require('autoprefixer')] } },
+                            { loader: 'sass-loader', options: { outputStyle: 'expanded', precision: 7 } }
+                        ]
+                    })
+                }, {
+                    test: /\.css$/,
+                    loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
+                },
+            ]
+        },
+        plugins: [
+            new ExtractTextPlugin('jquery-ui.css'),
+        ]
+    }
+];
