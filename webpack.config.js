9aac9c39fcf6c2d4021a58649c98b2cff93d6f4e
philwareham <phil@designhive.com>
2019-09-23 11:06:49 +0100

    Fix build

--- webpack.config.js
+++ webpack.config.js
@@ -4,50 +4,48 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const StyleLintPlugin = require('stylelint-webpack-plugin');
 
-module.exports = [
-    {
-        entry: {
-            'jquery-ui.min.css': './scss/jquery-ui.scss'
-        },
-        output: {
-            path: distDir,
-            filename: '.Trashes'
-        },
-        module: {
-            rules: [
-                {
-                    test: /\.(scss)$/,
-                    use: [
-                        MiniCssExtractPlugin.loader,
-                        // Translates CSS into CommonJS modules.
-                        { loader: 'css-loader', options: { importLoaders: 2 } },
-                        // Run postCSS actions.
-                        { loader: 'postcss-loader', options: { plugins: [require('autoprefixer')] } },
-                        // Compiles Sass to CSS.
-                        {
-                            loader: 'sass-loader',
-                            options: {
-                                implementation: require('sass'),
-                                outputStyle: 'expanded'
-                            }
+module.exports = {
+    entry: {
+        'jquery-ui.min.css': './scss/jquery-ui.scss'
+    },
+    output: {
+        path: distDir,
+        filename: '.Trashes'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(scss)$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    // Translates CSS into CommonJS modules.
+                    { loader: 'css-loader', options: { importLoaders: 2 } },
+                    // Run postCSS actions.
+                    { loader: 'postcss-loader', options: { plugins: [require('autoprefixer')] } },
+                    // Compiles Sass to CSS.
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            implementation: require('sass'),
+                            outputStyle: 'expanded'
                         }
-                    ]
-                },
-            ]
-        },
-        plugins: [
-            new CleanWebpackPlugin(),
-            new MiniCssExtractPlugin({
-                filename: 'jquery-ui.min.css'
-            }),
-            new StyleLintPlugin({
-                configFile: '.stylelintrc.yml',
-                syntax: 'scss',
-                context: 'scss',
-                files: '**/*.scss',
-                failOnError: false,
-                quiet: false
-            }),
+                    }
+                ]
+            },
         ]
-    }
-];
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: 'jquery-ui.min.css'
+        }),
+        new StyleLintPlugin({
+            configFile: '.stylelintrc.yml',
+            syntax: 'scss',
+            context: 'scss',
+            files: '**/*.scss',
+            failOnError: false,
+            quiet: false
+        }),
+    ]
+};
